791daca5edec26c41ac46b0588899fc4b82b7985
unknown <jnobr3c+github@gmail.com>
2026-03-15 21:51:03 -0300

    :sparkles: feat: endpoint findAll para Técnico

--- src/main/java/com/nobre/helpdesk/services/TecnicoService.java
+++ src/main/java/com/nobre/helpdesk/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.nobre.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,9 @@ public class TecnicoService {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> 
 	    	new ObjectnotFoundException("Objeto não encontrado! Id: " + id));
+	}
+
+	public List<Tecnico> findAll() {
+		return repository.findAll();
 	}
 }
