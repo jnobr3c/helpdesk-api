7cd02892ffcbd73a77b8554f450032979c24c778
unknown <jnobr3c+github@gmail.com>
2026-03-16 23:22:34 -0300

    :sparkles: feat: metodo PUT para Tecnico implementado

--- src/main/java/com/nobre/helpdesk/domain/Tecnico.java
+++ src/main/java/com/nobre/helpdesk/domain/Tecnico.java
@@ -36,7 +36,11 @@ public class Tecnico extends Pessoa {
 		this.cpf = obj.getCpf();
 		this.email = obj.getEmail();
 		this.senha = obj.getSenha();
-		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.perfis = obj.getPerfis()
+				.stream()
+				.map(x -> x
+						.getCodigo())
+				.collect(Collectors.toSet());
 		this.dataCriacao = obj.getDataCriacao();
 	}
 
--- src/main/java/com/nobre/helpdesk/services/TecnicoService.java
+++ src/main/java/com/nobre/helpdesk/services/TecnicoService.java
@@ -3,6 +3,8 @@ package com.nobre.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -40,6 +42,14 @@ public class TecnicoService {
 		return repository.save(newObj);
 	}
 
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+		objDTO.setId(id);
+		Tecnico oldObj = findById(id);
+		validaPorCpfEEmail(objDTO);
+		oldObj = new Tecnico(objDTO);
+		return repository.save(oldObj);
+	}
+
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
 		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
@@ -50,5 +60,6 @@ public class TecnicoService {
 		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
 			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
 		}		
-	} 
+	}
+ 
 }
