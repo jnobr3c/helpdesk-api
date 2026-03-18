36ce31474aac0fb7ab19fb0aad1ba74a2e735995
unknown <jnobr3c+github@gmail.com>
2026-03-17 22:45:05 -0300

    :sparkles: feat: Criar Construtor para UserSS com parametros

--- src/main/java/com/nobre/helpdesk/security/UserSS.java
+++ src/main/java/com/nobre/helpdesk/security/UserSS.java
@@ -1,17 +1,30 @@
 package com.nobre.helpdesk.security;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-public class UserSS implements UserDetails{
+import com.nobre.helpdesk.domain.enums.Perfil;
 
+public class UserSS implements UserDetails{
+	private static final long serialVersionUID = 1L;
+	
 	private Integer id;
 	private String email;
 	private String senha;
 	private  Collection<? extends GrantedAuthority> authorities;
 	
+	public UserSS(Integer id, String email, String senha, Set<Perfil> perfis) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.senha = senha;
+		this.authorities = perfis.stream().map(x -> new SimpleGrantedAuthority(x.getDescricao())).collect(Collectors.toSet());
+	}
+
 	public Integer getId() {
 		return id;
 	}
