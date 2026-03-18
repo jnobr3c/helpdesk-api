280ff61025e9a73a9e7d8cd4ac44097217dcb9f1
unknown <jnobr3c+github@gmail.com>
2026-03-17 22:39:04 -0300

    :sparkles: feat: Adicionar atributos id, email, senha e lista de authorities

---
+++ src/main/java/com/nobre/helpdesk/security/UserSS.java
@@ -0,0 +1,54 @@
+package com.nobre.helpdesk.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserSS implements UserDetails{
+
+	private Integer id;
+	private String email;
+	private String senha;
+	private  Collection<? extends GrantedAuthority> authorities;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
+
+}
